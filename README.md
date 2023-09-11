897369d886620275ad794498c9eaa1d466427f0e
Mücahit <mucahit.tezel@ogr.gelisim.edu.tr>
2023-09-11 14:21:04 +0300

    httpheaderscheck
    Bu Python aracı, HTTP yanıt başlıklarını denetleyerek eksik veya hatalı yapılandırılmış başlıkları tespit eder. Web uygulamanızın güvenliği ve performansı için kritik olan bu başlıkları kontrol eder ve düzeltmeniz için size yardımcı olur.

--- README.md
+++ README.md
@@ -1,2 +1,78 @@
-# HttpHeadersCheck
- Bu Python aracı, HTTP yanıt başlıklarını denetleyerek eksik veya hatalı yapılandırılmış başlıkları tespit eder. Web uygulamanızın güvenliği ve performansı için kritik olan bu başlıkları kontrol eder ve düzeltmeniz için size yardımcı olur.
+# Http Response Headers Check
+
+### Giriş - Tanım
+
+Bu Python tabanlı araç, HTTP yanıt başlıklarını kontrol etmek için tasarlanmıştır. HTTP, web tarayıcıları ve sunucular arasındaki iletişimde kullanılan temel bir protokoldür. Bu araç, verilen bir domain veya hedef IP adresinin HTTP yanıt başlıklarını inceleyerek eksik veya yanlış yapılandırılmış başlıkları tespit etmenizi sağlar. Bu başlıklar, web uygulamanızın güvenliği ve performansı için kritik öneme sahip olan ayarları içerir. Bu nedenle bu araç, web uygulamanızın sağlığını kontrol etmek ve düzeltmek için kullanabileceğiniz önemli bir yardımcıdır.
+
+
+## Kullanım
+
+Tool'u kullanmaya başlamak için aşağıdaki adımları izleyin:
+
+| Adım | Açıklama |
+| ---- | -------- |
+| 1    | Bu repository'yi klonlayın: `https://github.com/istanboolean/HttpResponseHeadersCheck.git` |
+| 2    | Klonladığınız dizine gidin: `cd HttpResponseHeadersCheck` |
+| 3    | cd ile içine girin , HttpResponseCheck dosyasına girin. |
+| 4    | dosya içini listelediğinizde "httpcheck.py" python dosyasını göreceksiniz. chmod +x ile dosyayı çalıştırılabilir duruma getirin.|
+| 5    | Gerekli Python kütüphanelerini yükleyin: `pip install -r requirements.txt` Kütüphaneler otomatik yüklenir.|
+
+
+                      Tool'u kullanın:    python3 httpcheck.py 
+<img width="1171" alt="httpcheck" src="https://github.com/istanboolean/HttpResponseHeadersCheck/assets/98133561/e2f96ed8-c6d5-4c99-a1b3-ed1e101b372e">
+
+                                                              
+
+## Parametreler
+
+  -Parametre-| -Açıklama-
+| -----------| ------------------------------------------------------------|
+-d DOMAIN    | Domain adresini belirlemek için kullanılır.
+| -----------| ------------------------------------------------------------|
+-t TARGET_IP | Hedef IP adresini belirlemek için kullanılır.
+| -----------| ------------------------------------------------------------|
+-p, --print | HTTP başlıklarını tablo şeklinde yazdırmak için kullanılır.
+| -----------| ------------------------------------------------------------|
+-c HEADER [HEADER ...] | Belirli başlıkları kontrol etmek için kullanılır.
+| -----------| ------------------------------------------------------------|
+-C | Tüm başlıkları kontrol etmek için kullanılır.
+| -----------| ------------------------------------------------------------|
+
+
+
+## Kullanım Örnekleri
+
+Aşağıda, HTTP başlıklarını kontrol etmek için kullanabileceğiniz farklı örnekleri bulabilirsiniz:
+
+| Örnek                                 | Komut                                          | Açıklama                                                               |
+|-----------------------------------------|----------------------------------------------|------------------------------------------------------------------------|
+| Domain kontrolü                         | `python3 httpcheck.py -d example.com`        | Belirtilen domain için HTTP başlıklarını kontrol eder.                 |
+| Hedef IP kontrolü                       | `python3 httpcheck.py -t 192.168.1.33`       | Belirtilen IP adresi için HTTP başlıklarını kontrol eder.              |
+| Domain kontrolü ve tabloyla görüntüleme | `python3 httpcheck.py -d example.com -p`     | HTTP başlıklarını tablo şeklinde görüntüler.                           |
+| Belirli başlıkları kontrol etme         | `python3 httpcheck.py -d example.com -c Server Date` | Belirli başlıkları (örneğin, "Server" ve "Date") kontrol eder. |
+| Tüm başlıkları kontrol etme             | `python3 httpcheck.py -d example.com -C`     | Tüm HTTP başlıklarını kontrol eder.                                    | 
+
+## Kod Çalışması
+
+
+Tool, "argparse" kütüphanesini kullanarak komut satırı argümanlarını işler ve kullanıcıdan girdi alır. "requests" kütüphanesini kullanarak HTTP istekleri gönderir ve yanıtları alır.
+  "sys" kütüphanesini kullanarak Python yürütme ortamını kontrol eder. "rich" kütüphanesini kullanarak zengin metin çıktısı oluşturur.
+
+Tool, belirtilen domain veya hedef IP adresine bir HTTP isteği gönderir. Yanıtın başlıkları, rich.table.Table sınıfı kullanılarak bir tablo şeklinde görüntülenir.
+
+## Kullanılan Kütüphaneler
+
+Bu aracı kullanmak için aşağıdaki Python kütüphanelerine ihtiyaç vardır:
+|----------------------------------------------------------------------------------------------------|
+| argparse:Komut satırı argümanlarını işlemek ve kullanıcıdan girdi almak için kullanılır.
+| requests:HTTP istekleri göndermek ve yanıtları almak için kullanılır.
+|sys: Python yürütme ortamını kontrol etmek için kullanılır.
+|rich.console.Console:Zengin metin çıktısı oluşturmak için kullanılır.
+|rich.table.Table:Tablo oluşturmak ve düzenlemek için kullanılır.
+|rich.box:Tablo ve diğer bileşenlerin çerçevesini biçimlendirmek için kullanılır.
+
+Ek Bilgiler
+
+Tool, HTTP yanıt başlıklarının tümünü veya belirli başlıklarını görüntülemek için kullanılabilir.
+Tool, tablo şeklinde zengin metin çıktısı oluşturur.
+Tool, Python 3.6 veya üzeri sürümlerde çalışır.
---
+++ httpcheck.py
@@ -0,0 +1,252 @@
+import argparse
+from rich.console import Console
+from rich.table import Table
+import requests
+
+print("""
+  _   _ _____ _____ ____     _   _ _____    _    ____  _____ ____  ____     ____ _   _ _____ ____ _  __
+ | | | |_   _|_   _|  _ \   | | | | ____|  / \  |  _ \| ____|  _ \/ ___|   / ___| | | | ____/ ___| |/ /
+ | |_| | | |   | | | |_) |  | |_| |  _|   / _ \ | | | |  _| | |_) \___ \  | |   | |_| |  _|| |   | ' / 
+ |  _  | | |   | | |  __/   |  _  | |___ / ___ \| |_| | |___|  _ < ___) | | |___|  _  | |__| |___| . \ 
+ |_| |_| |_|   |_| |_|      |_| |_|_____/_/   \_\____/|_____|_| \_\____/   \____|_| |_|_____\____|_|\_\_
+                                                                                                       
+                                                                            PurpleBox | istanboolean""")
+
+
+default_headers = [
+    "X-Content-Type-Options",
+    "X-Frame-Options",
+    "Content-Security-Policy",
+    "Strict-Transport-Security",
+    "X-XSS-Protection",
+    "Referrer-Policy",
+    "Feature-Policy",
+    "Content-Security-Policy-Report-Only",
+    "Cache-Control",
+    "Clear-Site-Data",
+    "X-Permitted-Cross-Domain-Policies",
+    "Expect-CT",
+    "Public-Key-Pins",
+    "X-Content-Security-Policy",
+    "X-Download-Options",
+    "X-DNS-Prefetch-Control",
+    "X-Robots-Tag",
+    "X-Request-ID",
+    "X-UA-Compatible",
+]
+
+# Argümanları tanımla
+parser = argparse.ArgumentParser(description="HTTP Response Headers Kontrol Aracı")
+parser.add_argument(
+    "-d",
+    "--domain",
+    metavar="DOMAIN",
+    help="HTTP başlıkları kontrol edilecek olan domain adresi",
+)
+parser.add_argument(
+    "-t",
+    "--target-ip",
+    metavar="TARGET_IP",
+    help="HTTP başlıkları kontrol edilecek olan hedef IP adresi",
+)
+parser.add_argument(
+    "-c",
+    "--check",
+    nargs="+",
+    metavar="HEADER",
+    help="HTTP başlıklarını kontrol etmek için başlık adları belirtin. Örn. -c Header1 Header2",
+)
+parser.add_argument(
+    "-C",
+    "--check-all",
+    action="store_true",
+    help="Tüm HTTP başlıklarını kontrol etmek için kullanın.",
+)
+
+# Argümanları işle
+args = parser.parse_args()
+
+# Kullanıcı dostu çıktı için Rich kütüphanesi kullan
+console = Console()
+
+def normalize_url(url):
+    if not url.startswith("http://") and not url.startswith("https://"):
+        # URL başında http:// veya https:// yoksa ekleyin
+        url = f"http://{url}"
+    return url
+
+def get_response_headers(url):
+    try:
+        url = normalize_url(url)  # URL'yi düzeltilmiş bir versiyonuyla güncelle
+        response = requests.get(url)
+        response_headers = response.headers
+        return response_headers
+    except requests.exceptions.RequestException as e:
+        console.print(f"[bold red]Hata:[/bold red] {e}")
+        return None
+
+def check_headers(headers_to_check, response_headers):
+    result_table = []
+    for header in headers_to_check:
+        if header in response_headers:
+            result_table.append((header, response_headers[header], ""))
+        else:
+            result_table.append((header, "Not found", get_header_description(header)))
+    return result_table
+
+def get_header_description(header):
+    descriptions = {
+       
+"X-Content-Type-Options":"""Tarayıcının dosyanın türünü değiştirmesini önler. Bu, potansiyel güvenlik açıklarını kapatır.
+Öneri: Bu başlığı etkinleştirin ve "nosniff" değeri ile kullanın. Örnek: X-Content-Type-Options: nosniff""",
+
+"X-Frame-Options":"""Web sitelerinin başka sitelerde çerçeve içinde görüntülenmesini engeller. Bu, sitenizin bütünlüğünü ve güvenliğini korur.
+Öneri: Bu başlığı etkinleştirin ve "SAMEORIGIN" değeri ile kullanın. Örnek: X-Frame-Options: SAMEORIGIN""",
+
+"Content-Security-Policy":"""Tarayıcıda yüklenebilecek kaynakları ve izin verilen eylemleri tanımlar. XSS gibi saldırılara karşı koruma sağlar.
+Öneri: Güvenlik politikanızı oluşturun ve bu başlığı etkinleştirin. Örnek: Content-Security-Policy: default-src 'self'; script-src 'self' 'unsafe-inline'""",
+
+"Strict-Transport-Security":"""Tarayıcının sadece HTTPS üzerinden iletişim kurmasını zorlar. Veri iletimi sırasında güvenliği artırır.
+Öneri: Bu başlığı etkinleştirin ve "max-age" değeri ile bir yıl gibi uzun bir süre belirleyin. Örnek: Strict-Transport-Security: max-age=31536000""",
+
+"X-XSS-Protection":"""Tarayıcılara XSS saldırılarına karşı otomatik koruma sağlar. Zararlı kodların yürütülmesini engeller.
+Öneri: Bu başlığı etkinleştirin ve "1; mode=block" değeri ile kullanın. Örnek: X-XSS-Protection: 1; mode=block""",
+
+"Referrer-Policy":"""Tarayıcıların hangi bilgileri referer olarak gönderebileceğini belirler. Gizliliği ve güvenliği artırır.
+Öneri: Bu başlığı etkinleştirin ve "no-referrer" veya "strict-origin" gibi uygun bir politika belirleyin. Örnek: Referrer-Policy: no-referrer""",
+
+"Feature-Policy":"""Belirli tarayıcı özelliklerinin kullanımını sınırlar. Güvenliği ve gizliliği artırır.
+Öneri: Tarayıcı özelliklerini sınırlamak için bu başlığı etkinleştirin ve uygun bir yapılandırmayı kullanın. Örnek: Feature-Policy: geolocation 'none'; camera 'self'""",
+
+"Content-Security-Policy-Report-Only":"""Politika ihlallerini raporlamak için kullanılır, ancak tarayıcıya bir politika uygulanmaz. Güvenlik politikalarının izlenmesine yardımcı olur.
+Öneri: Politika ihlali durumunda raporlar oluşturmak ve politikayı güncellemek için bu başlığı kullanabilirsiniz. Örnek: Content-Security-Policy-Report-Only: default-src 'self'; report-uri /csp-report-endpoint/""",
+
+"Cache-Control":"""Açıklama: Tarayıcıların sayfanın önbelleğe alınma şeklini kontrol etmesini sağlar.
+Öneri: Bu başlığı etkinleştirin ve önbellekleme davranışını belirleyin. Örnek: Cache-Control: no-cache, no-store, must-revalidate""",
+
+"Clear-Site-Data":"""Tarayıcıya belirli verilerin önbelleğini temizlemesi için talimat verir. Gizliliği korur.
+Öneri: Tarayıcı, belirli verileri temizlemek için bu başlığı kullanmalıdır. Örnek: Clear-Site-Data: 'cache', 'cookies'""",
+
+"X-Permitted-Cross-Domain-Policies":"""Adobe Flash uygulamalarının farklı alanlardan veri almasını ve göndermesini kontrol eder.
+Öneri: Bu başlığı etkinleştirin ve uygun bir yapılandırmayı kullanın. Örnek: X-Permitted-Cross-Domain-Policies: none""",
+
+"Expect-CT":"""Sertifika geçerliliğini kontrol etme politikalarını belirler. Sertifika sorunlarını tespit eder ve raporlar.
+Öneri: Bu başlığı etkinleştirin ve geçerlilik kontrol politikalarını belirleyin. Örnek: Expect-CT: enforce, max-age=3600""",  
+
+"Public-Key-Pins":"""Tarayıcılara belirli bir SSL/TLS sertifikasını kullanmalarını emreder. Güvenliği artırır.
+Öneri: Belirli bir sertifikayı kullanması gereken tarayıcıları belirtmek için bu başlığı etkinleştirin. Örnek: Public-Key-Pins: pin-sha256="base64+primary=="; max-age=5184000""",
+
+"X-Content-Security-Policy":"""İçerik güvenliği politikalarını tanımlar.
+Öneri: Tarayıcı, belirli içerik güvenliği politikalarını uygulamak için bu başlığı kullanmalıdır. Örnek: X-Content-Security-Policy: default-src 'self'""",
+
+"X-Download-Options":"""Dosya indirme işlemlerini belirler.
+Öneri: Tarayıcı, dosya indirme işlemlerini belirli bir şekilde işlemek için bu başlığı etkinleştirmelidir. Örnek: X-Download-Options: noopen""",
+
+"X-DNS-Prefetch-Control":"""DNS önbellekleme davranışını kontrol eder.
+Öneri: Tarayıcı, DNS önbellekleme davranışını yapılandırmaya izin veren bu başlığı etkinleştirmelidir. Örnek: X-DNS-Prefetch-Control: off""",
+
+"X-Robots-Tag":"""Web tarayıcılarına ve arama motorlarına sayfanın dizinlenip dizinlenmeyeceğini belirtir.
+Öneri: Sayfanızın dizinlenip dizinlenmeyeceğini belirlemek için bu başlığı etkinleştirin. Örnek: X-Robots-Tag: noindex, nofollow""",
+
+"X-Request-ID":"""İstekleri benzersiz bir şekilde tanımlar ve izler.
+Öneri: İstekleri izlemek ve yönetmek için bu başlığı kullanmalısınız. Örnek: X-Request-ID: unique-id""",
+
+"X-UA-Compatible":"""Tarayıcıların sayfa uyumluluk modunu belirler.
+Öneri: Tarayıcıların sayfanızın uyumluluk modunu doğru şekilde belirlemesi için bu başlığı etkinleştirin. Örnek: X-UA-Compatible: IE=edge"""
+
+    }
+    return descriptions.get(header, "Bu Başlık Kullanılıyor.")
+
+def main():
+    # Kullanım talimatlarını ve parametreleri göster.
+    if args.domain:
+        console.print(f"[bold green]Geçerli Domain:[/bold green] {args.domain}")
+        url = args.domain
+        response_headers = get_response_headers(url)
+        args.print= True
+        if response_headers and args.print:
+            console.print("\n[bold cyan]HTTP Response Headers[/bold cyan]")
+            table = Table(show_header=True, header_style="bold cyan")
+            table.add_column("Başlık", style="bold green")
+            table.add_column("Değer", style="bold white")
+            for key, value in response_headers.items():
+                table.add_row(key, value)
+            console.print(table)
+        if args.check or args.check_all:
+            headers_to_check = args.check if args.check else default_headers
+            checked_headers = check_headers(headers_to_check, response_headers)
+            console.print("\n[bold cyan]Başlık Kontrolleri[/bold cyan]")
+            table = Table(show_header=True, header_style="bold cyan")
+            table = Table(show_lines=True, header_style="bold cyan")
+            table.add_column("Başlık", style="bold red")
+            table.add_column("Değer", style="bold yellow")
+            table.add_column("Açıklama", style="bold white")
+            for header, value, description in checked_headers:
+                if header in response_headers:
+                    continue
+                else:
+                    table.add_row(header, value, description)
+                
+            console.print(table)
+            
+    elif args.target_ip:
+        console.print(f"[bold green]Geçerli Hedef IP:[/bold green] {args.target_ip}")
+        url = f"http://{args.target_ip}"
+        response_headers = get_response_headers(url)
+        args.print = True
+        if response_headers and args.print:
+            console.print("\n[bold cyan]HTTP Response Headers[/bold cyan]")
+            table = Table(show_header=True, header_style="bold cyan")
+            table.add_column("Başlık", style="bold green")
+            table.add_column("Değer", style="bold white")
+            for key, value in response_headers.items():
+                table.add_row(key, value)
+            console.print(table)
+        if args.check or args.check_all:
+            headers_to_check = args.check if args.check else default_headers
+            checked_headers = check_headers(headers_to_check, response_headers)
+            console.print("\n[bold cyan]Başlık Kontrolleri[/bold cyan]")
+            table = Table(show_header=True, header_style="bold cyan")
+            table = Table(show_lines=True, header_style="bold cyan")
+            table.add_column("Başlık", style="bold red")
+            table.add_column("Değer", style="bold yellow")
+            table.add_column("Açıklama", style="bold white")
+            for header, value, description in checked_headers:
+                if header in response_headers:
+                    continue
+                else:
+                    table.add_row(header, value, description)
+                
+                
+            console.print(table)
+    else:
+        console.print("[bold cyan]KOD KULLANIMI[/bold cyan]")
+        console.print("=" * 30)
+        console.print(
+            """Bu tool, belirtilen bir domain veya hedef IP adresi için HTTP yanıt başlıklarını görüntüler.
+Aynı zamanda eksik ve misconfigured headers hakkında çözüm önerileri sunar."""
+        )
+        console.print("\n[bold cyan]PARAMETRELER VE KULLANIMI[/bold cyan]")
+        console.print("=" * 45)
+        table = Table(show_header=True, header_style="bold cyan")
+        table.add_column("Parametre", style="bold red")
+        table.add_column("Açıklama", style="bold white")
+        table.add_row("-h ", "Help kütüphanesini getirir.")
+        table.add_row("-d DOMAIN", "Domain adresini belirlemek için kullanılır.")
+        table.add_row("-t TARGET_IP", "Hedef IP adresini belirlemek için kullanılır.")
+        table.add_row("-c HEADER [HEADER ...]", "Belirli başlıkları kontrol etmek için kullanılır.")
+        table.add_row("-C", "Tüm başlıkları kontrol etmek için kullanılır.")
+        console.print(table)
+        console.print("\n[bold cyan]KULLANIM ÖRNEKLERİ[/bold cyan]")
+        console.print("=" * 30)
+        table = Table(show_header=True, header_style="bold cyan")
+        table.add_column("Kullanım Örneği", style="bold red")
+        table.add_column("Komut", style="bold white")
+        table.add_row("Domain kontrolü örneği", "python3 httpcheck.py -d www.example.com")
+        table.add_row("Hedef IP kontrolü örneği", "python3 httpcheck.py -t 192.168.1.1")
+        table.add_row("Belirli başlıkları kontrol etme örneği", "python3 httpcheck.py -d www.example.com -c Server Date")
+        table.add_row("Tüm başlıkları kontrol etme örneği", "python3 httpcheck.py -d example.com -C")
+        console.print(table)
+
+if __name__ == "__main__":
+    main()
